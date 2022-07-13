05286cb15546bc44b1659468f868dc7f53687e35
LuaDlc <luadiaslc@gmail.com>
2022-07-13 14:39:15 -0300

    atualização

--- sign_example/lib/main.dart
+++ sign_example/lib/main.dart
@@ -63,6 +63,28 @@ class _SignUpFormState extends State<SignUpForm> {
   final _lastNameTextController = TextEditingController();
   final _usernameTextController = TextEditingController();
 
+
+  //Este método atualiza o campo _formProgress com base no número de campos de texto não vazios
+void _updateFormProgress() {
+  var progress = 0.0;
+  final controllers = [
+    _firstNameTextController,
+    _lastNameTextController,
+    _usernameTextController
+  ];
+
+  for (final controller in controllers) {
+    if (controller.value.text.isNotEmpty) {
+      progress += 1 / controllers.length;
+    }
+  }
+
+  setState(() {
+    _formProgress = progress;
+  });
+}
+
+
   double _formProgress = 0;
 void _showWelcomeScreen() {
              Navigator.of(context).pushNamed('/welcome');
@@ -70,6 +92,7 @@ void _showWelcomeScreen() {
   @override
   Widget build(BuildContext context) {
     return Form(
+      onChanged: _updateFormProgress,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
@@ -109,7 +132,8 @@ void _showWelcomeScreen() {
                 return states.contains(MaterialState.disabled) ? null : Colors.blue;
               }),
             ),
-            onPressed: _showWelcomeScreen,
+            
+            onPressed: _formProgress == 1 ? _showWelcomeScreen : null,
             child: const Text('Sign up'),
           ),
         ],
